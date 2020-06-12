293b1f2629ccad843ce52083e65f16024846fbb1
TimChen <b05601005@ntu.edu.tw>
2020-06-12 19:24:38 +0800

    initailize project template

---
+++ cmdParser.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <sstream>
+#include <stack>
+#include <cassert>
+#include "./mailDB.h"
+
+using namespace std;
+
+void split(const string& str, vector<string>& cont, char delim = ' ')
+{
+    stringstream ss(str);
+    string token;
+    cont.clear();
+    while (getline(ss, token, delim)) {
+        cont.push_back(token);
+    }
+}
+
+int main() 
+{
+    string inputLine;
+    vector<string> lineSplit;
+
+    MailDB mailDB;
+
+    while(getline(cin, inputLine)){
+        if(inputLine.size()==0) break;
+        split(inputLine, lineSplit);
+        if (lineSplit[0]=="add") {
+            mailDB.add(lineSplit[1]);
+        } 
+        else if (lineSplit[0]=="remove") {
+            unsigned id = stoi(lineSplit[1]);
+            mailDB.remove(id);
+        }
+        else if (lineSplit[0]=="longest") {
+            mailDB.longest();
+        }
+        else if (lineSplit[0]=="query") {
+            vector<string> args(lineSplit.begin()+1, lineSplit.end());
+            mailDB.query(args);
+        }
+        inputLine.clear();
+    }
+}
---
+++ mailDB.h
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <unordered_set>
+#include <set>
+#include <string>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <queue>
+
+typedef std::pair<int, unsigned> LENGTH; 
+
+class Mail
+{
+public: 
+    bool searchContent(std::string& keyword);
+
+    // Enable MailDB access private data
+    friend class MailDB;
+private:
+    unsigned id;
+    std::string subject;
+    std::string from;
+    std::string to;
+    // TO-Do:
+    // A container for content
+};
+
+class MailDB
+{
+public:
+    MailDB() {};
+    ~MailDB() {};
+
+    // Thers four member funtions is the interface of this class
+    // which is designed for the four commands respectively
+    void add(std::string& path);
+    void remove(unsigned id);
+    void longest();
+    void query(std::vector<std::string>& args) 
+    {
+        if (args.size()>1) 
+            queryWithCond(args);
+        else if (args.size()==1)
+            queryOnlyExpr(args[0]);
+    }
+private:
+    // We may need more member data or function hear
+
+    // These two private function is for two query situation
+    // 1. query only with expression
+    // 2. query with other conditions
+    // You can costumize your situation
+    // and don't forget to change the interface too!
+    void queryOnlyExpr(std::string& expr);
+    void queryWithCond(std::vector<std::string>& args);
+
+    // This heap is for the longest() function
+    std::priority_queue<LENGTH, std::vector<LENGTH>, std::greater<int> > lengthHeap;
+    
+    // TO-Do:
+    // A container store mails
+    // and maybe other containers to store content
+};
